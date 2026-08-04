343c897626e5d375856bab2d19a3778f2bd5ebdf
Kerrzo <pfkerr@gmail.com>
2026-08-04 18:19:54 +1000

    Fix site URL for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kerrzo.com',
+  site: 'https://kerrzo.github.io/brand',
   output: 'static',
   trailingSlash: 'always',
   integrations: [sitemap()],
